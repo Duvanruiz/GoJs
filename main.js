0f72cfb64ea1f11044447a2c752e69a759e577bb
Gabr1elaMR7 <f.e.hvrcgmr@gmail.com>
2024-05-02 10:53:54 -0500

    imagenes en nodos

--- main.js
+++ main.js
@@ -42,11 +42,8 @@ function init() {
       { key: "RACK1_CAJA_OB_1" },
       { key: "RACK1_CAJA_OB_2" },
       { key: "NGWESPACIOSUR"},
-      { key: "HAC-VAL.YUMBO2_MOV-CP1" },
-      { key: "NGWCALINORTE" },
-      { key: "R1-F1 ODF2" },
-      { key: "R1-F1 ODF1" },
-      { key: "102-01B-40" }
+      { key: "HAC-VAL.YUMBO2_MOV-CP1"},
+      { key: "R1-F1 ODF2"}
     ];
   
     model.linkDataArray = [
@@ -55,13 +52,7 @@ function init() {
       { from: "ISWFTTHSABANΕΤΑΑΤΡ", to: "RACK1_CAJA_OB_1",port:"100GE0/0/1 -> Hilo 1 y 2" },
       { from: "ISWFTTHSABANΕΤΑΑΤΡ", to: "RACK1_CAJA_OB_2",port:"100GE1/0/1 -> Hilo 1 y 2" },
       { from: "RACK1_CAJA_OB_1", to: "NGWESPACIOSUR", port:"null -> 50|100GE9/0/4" },
-      { from: "RACK1_CAJA_OB_2", to: "NGWESPACIOSUR",port:"null -> 50|100GE9/0/4"},
-      { from: " HAC-VAL.YUMBO2_MOV-CP1", to: "R1-F1 ODF2", port:"0/8/2 -> Gi15/1/3" },
-      { from: " HAC-VAL.YUMBO2_MOV-CP1", to: "R1-F1 ODF1", port:"0/9/2 -> Gi15/1/12" },
-      { from: "R1-F1 ODF2", to: "102-01B-40",port:"B1/B2 -> B1/B2" },
-      { from: "R1-F1 ODF1", to: "105-06B-70",port:"A8/A8 -> A8/A8" },
-      { from: "102-01B-40", to: "NGWCALINORTE", port:"B7/B8 -> B7/B8" },
-      { from: "105-06B-70", to: "NGWCALINORTE",port:"D3/D4-> D3/D4"}
+      { from: "RACK1_CAJA_OB_2", to: "NGWESPACIOSUR",port:"null -> 50|100GE9/0/4"}
     ];
 
    
